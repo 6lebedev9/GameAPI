f5367e54676c980060d675f1c5262569dac90912
6lebedev9 <6lebedev9@gmail.com>
2025-04-15 10:14:27 +0300

    fixed auto-increment in tgid

--- Models/Token.cs
+++ Models/Token.cs
@@ -1,14 +1,22 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GameAPI.Models
 {
     public class Token
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public long TgId { get; set; }
+
         public long ChatId { get; set; }
+
+        [Required]
+        [StringLength(5)]
         public string TgToken { get; set; } = string.Empty;
+
         public DateTime ExpiredAt { get; set; }
+
         public bool IsUsed { get; set; } = false;
     }
 }
